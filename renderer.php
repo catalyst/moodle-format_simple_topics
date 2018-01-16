32f54363ccc1dff98539e379c2630a399b7e0f3a
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2018-01-16 11:38:30 +1100

    Rewrite section summary render

--- renderer.php
+++ renderer.php
@@ -31,18 +31,48 @@ require_once($CFG->dirroot.'/course/format/topics/renderer.php');
 class format_simple_topics_renderer extends format_topics_renderer {
 
     /**
-     * Generate a summary of the activites in a section.
+     * Generate a summary of a section for display on the 'course index page'
      *
      * @param stdClass $section The course_section entry from DB
-     * @param stdClass $course the course record from DB
+     * @param stdClass $course The course entry from DB
      * @param array    $mods (argument not used)
-     *
      * @return string HTML to output.
      */
-    protected function section_activity_summary($section, $course, $mods) {
-        // Don't show any summary.
-        return '';
-    }
+    protected function section_summary($section, $course, $mods) {
+        $classattr = 'section main section-summary clearfix';
+        $linkclasses = '';
 
+        // If section is hidden then display grey section link.
+        if (!$section->visible) {
+            $classattr .= ' hidden';
+            $linkclasses .= ' dimmed_text';
+        } else if (course_get_format($course)->is_section_current($section)) {
+            $classattr .= ' current';
+        }
+
+        $titletext = get_section_name($course, $section);
+        $title = $this->output->heading($titletext, 3, 'section-title');
+
+        $number = html_writer::tag('div', $section->section, array('class' => 'section-number'));
+        $tick = html_writer::tag('div', '', array('class' => 'section-tick'));
+
+        $o = '';
+        $o .= html_writer::start_tag('li', array('id' => 'section-' . $section->section,
+            'class' => $classattr, 'role' => 'region', 'aria-label' => $titletext));
+
+        $o .= html_writer::tag('div', '', array('class' => 'left side'));
+        $o .= html_writer::tag('div', '', array('class' => 'right side'));
+        $o .= html_writer::start_tag('div', array('class' => 'content'));
+
+        if ($section->uservisible) {
+            $o .= html_writer::tag('a', $title . $number . $tick,
+                array('href' => course_get_url($course, $section->section), 'class' => $linkclasses));
+        }
+
+        $o .= html_writer::end_tag('div');
+        $o .= html_writer::end_tag('li');
+
+        return $o;
+    }
 
 }
