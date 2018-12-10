a90d921c685f683218ec89468c5e8764df1c078f
Kristian Ringer <kristian.ringer@catalyst-au.net>
2018-12-10 11:52:36 +1100

    Add config option for displaying inaccessible topics

--- settings.php
+++ settings.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,23 +15,19 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings
- *
- * @package    format
- * @subpackage simple_topics
- * @author     Dmitrii Metelkin (dmitriim@catalyst-au.net)
- * @copyright  Catalyst IT
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   simple_topics_format
  */
 
+// This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Simple Topics format';
-$string['sectionname'] = 'Topic';
-$string['hidefromothers'] = 'Hide topic';
-$string['showfromothers'] = 'Show topic';
-$string['prevunit'] = 'Previous Unit:';
-$string['nextunit'] = 'Next Unit:';
-$string['displayhiddentopics'] = 'Hide/display inaccessible topics';
-$string['displayhiddentopicsdesc'] = 'Display inaccessible topics greyed out if the user cannot access them. If disabled, these topics will be hidden from view.';
+// This is used for performance, we don't need to know about these settings on every page in Moodle, only when
+// we are looking at the admin settings pages.
+if ($ADMIN->fulltree) {
 
+     //Config option for displaying inaccessible topics in a course, or hiding them from view.
+    $name = 'format_simple_topics/displayhiddentopics';
+    $title = get_string('displayhiddentopics', 'format_simple_topics');
+    $description = get_string('displayhiddentopicsdesc', 'format_simple_topics');
+    $settings->add(new admin_setting_configcheckbox($name, $title, $description, 0));
+}
