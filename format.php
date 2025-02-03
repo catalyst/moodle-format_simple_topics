d0136fac59d489d20f30509bede9c879af43b23c
ScottVerbeek <scottverbeek@catalyst-au.net>
2025-02-03 12:42:44 +1000

    Remove deprecated format.js
    For more info see tracker https://tracker.moodle.org/browse/MDL-74778

--- format.php
+++ format.php
@@ -50,6 +50,3 @@ if (!empty($displaysection)) {
 $outputclass = $format->get_output_classname('content');
 $widget = new $outputclass($format);
 echo $renderer->render($widget);
-
-// Include course format js module
-$PAGE->requires->js('/course/format/topics/format.js');
